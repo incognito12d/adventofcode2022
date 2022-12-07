a82e2d331b0d9dd64333a9455c5d7343efe46693
Lars Ericson <lars.ericson@guardtools.com>
2022-12-07 19:43:14 +0100

    Added Day 6 part 1 solution.

---
+++ Day6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var buffer = System.IO.File.ReadAllText(@"C:\AdventOfCode\6\input.txt");
+
+            var charList = new List<char>();
+            var markerIndexCounter = 0;
+            var startIndex = 0;
+            for (var index = 0; index < buffer.Length; index++)
+            {
+                var c = buffer[index];
+                if (!charList.Contains(c))
+                {
+                    charList.Add(c);
+                    markerIndexCounter++;
+                    if (markerIndexCounter == 4)
+                    {
+                        startIndex = index;
+                        break;
+                    }
+                }
+                else
+                {
+                    charList.Clear();
+                    markerIndexCounter = 0;
+                }
+
+            }
+            Console.WriteLine($"Index: {startIndex}");
+        }
+    }
+}
